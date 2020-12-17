1fd4f082f4bc8a5180698b8bfd011a2214201ef9
FinnRules <finnrules23@gmail.com>
2020-12-17 16:38:52 -0500

    Created mod.conf, removed all dependencies, added support for two games, fixed bug in cropapi

---
+++ functions.lua
@@ -0,0 +1,128 @@
+overproduced.place_seed = function(itemstack, placer, pointed_thing, plantname)
+	local pt = pointed_thing
+	-- check if pointing at a node
+	if not pt then
+		return itemstack
+	end
+	if pt.type ~= "node" then
+		return itemstack
+	end
+
+	local under = minetest.get_node(pt.under)
+	local above = minetest.get_node(pt.above)
+
+	local player_name = placer and placer:get_player_name() or ""
+
+	if minetest.is_protected(pt.under, player_name) then
+		minetest.record_protection_violation(pt.under, player_name)
+		return
+	end
+	if minetest.is_protected(pt.above, player_name) then
+		minetest.record_protection_violation(pt.above, player_name)
+		return
+	end
+
+	-- return if any of the nodes is not registered
+	if not minetest.registered_nodes[under.name] then
+		return itemstack
+	end
+	if not minetest.registered_nodes[above.name] then
+		return itemstack
+	end
+
+	-- check if pointing at the top of the node
+	if pt.above.y ~= pt.under.y+1 then
+		return itemstack
+	end
+
+	-- check if you can replace the node above the pointed node
+	if not minetest.registered_nodes[above.name].buildable_to then
+		return itemstack
+	end
+
+	-- check if pointing at soil
+	if minetest.get_item_group(under.name, "soil") < 2 then
+		return itemstack
+	end
+
+	-- add the node and remove 1 item from the itemstack
+	minetest.log("action", player_name .. " places node " .. plantname .. " at " ..
+		minetest.pos_to_string(pt.above))
+	minetest.add_node(pt.above, {name = plantname, param2 = 1})
+	overproduced.tick(pt.above)
+	if not minetest.is_creative_enabled(player_name) then
+		itemstack:take_item()
+	end
+	return itemstack
+end
+
+overproduced.grow_plant = function(pos, elapsed)
+	local node = minetest.get_node(pos)
+	local name = node.name
+	local def = minetest.registered_nodes[name]
+
+	if not def.next_plant then
+		-- disable timer for fully grown plant
+		return
+	end
+
+	-- grow seed
+	if minetest.get_item_group(node.name, "seed") and def.fertility then
+		local soil_node = minetest.get_node_or_nil({x = pos.x, y = pos.y - 1, z = pos.z})
+		if not soil_node then
+			overproduced.tick_again(pos)
+			return
+		end
+		-- omitted is a check for light, we assume seeds can germinate in the dark.
+		for _, v in pairs(def.fertility) do
+			if minetest.get_item_group(soil_node.name, v) ~= 0 then
+				local placenode = {name = def.next_plant}
+				if def.place_param2 then
+					placenode.param2 = def.place_param2
+				end
+				minetest.swap_node(pos, placenode)
+				if minetest.registered_nodes[def.next_plant].next_plant then
+					overproduced.tick(pos)
+					return
+				end
+			end
+		end
+
+		return
+	end
+
+	-- check if on wet soil
+	local below = minetest.get_node({x = pos.x, y = pos.y - 1, z = pos.z})
+	if minetest.get_item_group(below.name, "soil") < 3 then
+		overproduced.tick_again(pos)
+		return
+	end
+
+	-- check light
+	local light = minetest.get_node_light(pos)
+	if not light or light < def.minlight or light > def.maxlight then
+		overproduced.tick_again(pos)
+		return
+	end
+
+	-- grow
+	local placenode = {name = def.next_plant}
+	if def.place_param2 then
+		placenode.param2 = def.place_param2
+	end
+	minetest.swap_node(pos, placenode)
+
+	-- new timer needed?
+	if minetest.registered_nodes[def.next_plant].next_plant then
+		oveproduced.tick(pos)
+	end
+	return
+end
+
+overproduced.tick = function(pos)
+	minetest.get_node_timer(pos):start(math.random(166, 286))
+end
+-- how often a growth failure tick is retried (e.g. too dark)
+overproduced.tick_again = function(pos)
+	minetest.get_node_timer(pos):start(math.random(40, 80))
+end
---
+++ minetest_game/crops.lua
@@ -0,0 +1,149 @@
+--Red Onion
+overproduced.register_plant("overproduced:red_onion", {
+	description = ("Red Onion Seed"),
+	harvest_description = ("Red Onion"),
+	paramtype2 = "meshoptions",
+	inventory_image = "overproduced_red_onion_seeds.png",
+	steps = 12,
+	minlight = 10,
+	maxlight = default.LIGHT_MAX,
+	fertility = {"grassland"},
+	groups = {flammable = 1},
+	place_param2 = 1,
+})
+
+--Yellow Onion
+overproduced.register_plant("overproduced:yellow_onion", {
+	description = ("Yellow Onion Seed"),
+	harvest_description = ("Yellow Onion"),
+	paramtype2 = "meshoptions",
+	inventory_image = "overproduced_yellow_onion_seeds.png",
+	steps = 12,
+	minlight = 10,
+	maxlight = default.LIGHT_MAX,
+	fertility = {"grassland"},
+	groups = {flammable = 1},
+	place_param2 = 1,
+})
+
+--White Onion
+overproduced.register_plant("overproduced:white_onion", {
+	description = ("White Onion Seed"),
+	harvest_description = ("White Onion"),
+	paramtype2 = "meshoptions",
+	inventory_image = "overproduced_white_onion_seeds.png",
+	steps = 12,
+	minlight = 10,
+	maxlight = default.LIGHT_MAX,
+	fertility = {"grassland"},
+	groups = {flammable = 1},
+	place_param2 = 1,
+})
+--[[
+--Peanuts
+--The seed is Raw peanuts, the crop is unshelled raw peanuts, they can be cooked via ffoods to create roasted peanuts and roasted unshelled peanuts which craft into roasted peanuts
+overproduced.register_plant("overproduced:peanuts", {
+	description = ("Raw Peanuts"),
+	harvest_description = ("Unshelled Raw Peanuts"),
+	paramtype2 = "meshoptions",
+	inventory_image = "overproduced_raw_peanuts.png",
+	steps = 5,
+	minlight = 10,
+	maxlight = default.MAX_LIGHT,
+	fertility = {"grassland"},
+	groups = {flammable = 2},
+	place_param2 = 1,
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "overproduced:seed_peanuts",
+	recipe = {"overproduced:peanuts"},
+	replacements = {
+		{"overproduced:peanuts", "overproduced:peanut_shells"}
+	}
+})
+
+minetest.register_craftitem("overproduced:peanut_shells", {
+	description = ("Peanut Shells"),
+	inventory_image = "overproduced_peanut_shells.png",
+})
+]]
+--Garlic
+overproduced.register_plant("overproduced:garlic", {
+	description = "Garlic Cloves",
+	harvest_description = "Garlic",
+	paramtype2 = "meshoptions",
+	inventory_image = "overproduced_garlic_cloves.png",
+	steps = 5,
+	minlight = 10,
+	maxlight = default.MAX_LIGHT,
+	fertility = {"grassland"},
+	groups = {flammable = 1},
+	place_param2 = 1,
+	seeddrop = false,
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "overproduced:seed_garlic 2",
+	recipe = {"overproduced:garlic"},
+})
+
+--Honeydew
+overproduced.register_melon("overproduced:honeydew", {
+	description = "Honeydew seeds",
+	harvest_description = "Honeydew",
+	paramtype2 = "meshoptions",
+	inventory_image = "overproduced_honeydew.png",
+	steps = 2,
+	minlight = 5,
+	maxlight = default.MAX_LIGHT,
+	fertility = {"grassland"},
+	groups =  {flammable = 1},
+	place_param2 = 1,
+	melon_tiles = {"overproduced_honeydew.png"},
+})
+
+--Rainbow Carrots
+overproduced.register_plant("overproduced:rainbow_carrots", {
+	description = "Rainbow Carrot Seeds",
+	harvest_description = "Rainbow Carrots",
+	paramtype2 = "meshoptions",
+	inventory_image = "overproduced_rainbow_carrots.png",
+	steps = 3,
+	minlight = 6,
+	maxlight = default.MAX_LIGHT,
+	fertility = {"grassland"},
+	groups = {},
+	place_param2 = 1,
+})
+
+--Habanero
+overproduced.register_plant("overproduced:habanero", {
+	description = "Habanero Seeds",
+	harvest_description = "Habanero",
+	paramtype2 = "meshoptions",
+	inventory_image = "overproduced_habanero.png",
+	steps = 4,
+	minlight = 6,
+	maxlight = default.MAX_LIGHT,
+	fertility = {"grassland"},
+	groups = {},
+	place_param2 = 1,
+	seeddrop = false,
+})
+
+overproduced.register_plant("overproduced:potato", {
+	description = "Potato",
+	harvest_description = "Potato_1",
+	paramtype2 = "meshoptions",
+	inventory_image = "overproduced_potato.png",
+	steps = 4,
+	minlight = 4,
+	maxlight = default.MAX_LIGHT,
+	fertility = {"grassland"},
+	groups = {},
+	place_param2 = 2,
+	seeddrop = "seed_only",
+})
